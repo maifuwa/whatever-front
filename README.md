69a883c4b98c5b3b9ec234d29d6da47e2ed3ccd9
maifuwa <maifuqla@gmail.com>
2023-11-08 15:20:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# whatever的前端界面
+
+这是v2版本，使用第三方组件库`naive ui`和新的`css`预处理器`tailwindcss`
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="w-32 h-32 bg-blue-500">你好</div>
+</template>
+
+<style scoped>
+
+</style>
